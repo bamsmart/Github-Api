e00499190256df194a9e4c647024e13f1260fcda
Bambang T. Lasiman <bambang.tryono@aladinbank.id>
2022-11-16 21:41:36 +0700

    add progurad

--- core/src/main/java/net/shinedev/core/di/UserDatabaseModule.kt
+++ core/src/main/java/net/shinedev/core/di/UserDatabaseModule.kt
@@ -9,6 +9,8 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import net.shinedev.core.data.source.local.room.UserDao
 import net.shinedev.core.data.source.local.room.UserDatabase
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 import javax.inject.Singleton
 
 @Module
@@ -17,13 +19,19 @@ class UserDatabaseModule {
 
     @Singleton
     @Provides
-    fun provideUserDatabase(@ApplicationContext context: Context): UserDatabase = Room.databaseBuilder(
-        context.applicationContext,
-        UserDatabase::class.java,
-        DB_NAME
-    ).allowMainThreadQueries()
-        .fallbackToDestructiveMigration()
-        .build()
+    fun provideUserDatabase(@ApplicationContext context: Context): UserDatabase {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("shinesdev".toCharArray())
+        val factory = SupportFactory(passphrase)
+
+        return Room.databaseBuilder(
+            context.applicationContext,
+            UserDatabase::class.java,
+            DB_NAME
+        ).allowMainThreadQueries()
+            .fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
+            .build()
+    }
 
     @Provides
     fun provideUserDao(database: UserDatabase): UserDao = database.getUserDao()
